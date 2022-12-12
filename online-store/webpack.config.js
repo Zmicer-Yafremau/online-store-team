aee33e723d1fa541cdfb8456579cc04de3fcd84f
Zmicer-Yafremau <dmitry.mickevich@gmail.com>
2022-12-12 22:20:45 +0300

    feat: add webpack and basic file structure

---
+++ online-store/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+const CopyWebpackPlugin = require ('copy-webpack-plugin');
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+                
+            },
+            { test: /\.ts$/i, use: 'ts-loader' },
+            {
+                test: /\.(jpg|svg|png|jpeg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]'
+                    },
+            },
+       
+        ],
+    },
+    resolve: {
+        extensions: ['.js','.ts'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new EslintPlugin({ extensions: 'ts' }),
+        new CopyWebpackPlugin ({
+            patterns: [
+                { from: 'src/assets', to: 'assets' }
+        ]
+    }),
+   
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
+1) npm run start - to run 
+(case mistake type in command line: export NODE_OPTIONS=--openssl-legacy-provider);
+2) npm run test - for tests;
+3) npm run lint - for checking with lint;
+4) npm run build - to create dist;
 
+In case of mistakes while pushing, type in command line: unset NODE_OPTIONS
