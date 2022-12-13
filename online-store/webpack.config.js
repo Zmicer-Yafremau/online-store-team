2b5bff3458060d9e07d51c9807ead62b20735c64
IngaMuse <ingamoshkareva@gmail.com>
2022-12-13 22:47:14 +0300

    feat: add webpack settings

--- online-store/webpack.config.js
+++ online-store/webpack.config.js
@@ -3,16 +3,27 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const EslintPlugin = require('eslint-webpack-plugin');
-const CopyWebpackPlugin = require ('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const pages = ['index', 'product-details', 'cart'];
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index'),
+    entry: pages.reduce((config, page) => {
+        config[page] = `./src/${page}`;
+        return config;
+    }, {}),
+    optimization: {
+        splitChunks: {
+        chunks: "all",
+        },
+    },
     mode: 'development',
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
                 
             },
             { test: /\.ts$/i, use: 'ts-loader' },
@@ -23,7 +34,6 @@ const baseConfig = {
                     name: '[path][name].[ext]'
                     },
             },
-       
         ],
     },
     resolve: {
@@ -34,10 +44,6 @@ const baseConfig = {
         path: path.resolve(__dirname, './dist'),
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html',
-        }),
         new CleanWebpackPlugin(),
         new EslintPlugin({ extensions: 'ts' }),
         new CopyWebpackPlugin ({
@@ -45,8 +51,23 @@ const baseConfig = {
                 { from: 'src/assets', to: 'assets' }
         ]
     }),
-   
-    ],
+    ]
+    .concat(
+        pages.map(
+        (page) =>
+            new HtmlWebpackPlugin({
+            inject: true,
+            template: `./src/${page}.html`,
+            filename: `${page}.html`,
+            chunks: [page],
+            })
+        )
+    ).concat(
+        new MiniCssExtractPlugin({
+        filename: `[name].css`,
+        chunkFilename: '[id].css',
+        }),
+    ),
 };
 
 module.exports = ({ mode }) => {
