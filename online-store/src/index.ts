b233dc603848bc21850b370a15bd57ed780b8cea
Zmicer <62570834+Zmicer-Yafremau@users.noreply.github.com>
2022-12-15 10:19:44 +0300

    Feat/add cards (#5)
    * feat: import bootstrap
    
    * feat: add cards

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,2 +1,6 @@
-import './global.css';
+import './global.scss';
 import '../node_modules/normalize.css/normalize.css';
+import 'bootstrap';
+import { CARDS } from './components/cards/cards';
+import { fill } from './components/fill';
+fill(CARDS);
