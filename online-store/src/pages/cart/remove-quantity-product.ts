dbc5e125f41687e132d888b3246e709d2d53af7c
IngaMuse <ingamoshkareva@gmail.com>
2022-12-25 23:55:42 +0300

    feat: add buttons logic

---
+++ online-store/src/pages/cart/remove-quantity-product.ts
@@ -0,0 +1,54 @@
+import { CARDS } from "../../components/cards/cards";
+import { cardType } from "../../types/types";
+
+export function removeQuantity(
+    CART: HTMLSpanElement,
+    TOTAL_SUM: HTMLSpanElement,
+    CARD_ID: string,
+    BUTTON: HTMLButtonElement,
+    STOCK: HTMLCollectionOf<HTMLSpanElement>,
+    QUANTITY: HTMLCollectionOf<HTMLSpanElement>,
+    SUMMARY_PRODUCT: HTMLSpanElement,
+    SUMMARY_TOTAL: HTMLSpanElement,    
+    REMOVE_ITEM: HTMLDivElement
+): () => void {
+    return () => {
+        if (!localStorage.basket) {
+            CART.classList.add('visually-hidden');
+            localStorage.basket = `[]`;
+            TOTAL_SUM.innerHTML = '0';
+        }
+        let ID_ARR: string[] = JSON.parse(localStorage.basket);
+        const STOCK_SET = [...new Set(ID_ARR)].indexOf(CARD_ID);
+        if (Number(QUANTITY[STOCK_SET].textContent)>1) {
+            const positionID = ID_ARR.lastIndexOf(CARD_ID);
+            ID_ARR.splice(positionID, 1);
+            STOCK[STOCK_SET].textContent = `${Number(STOCK[STOCK_SET].textContent) + 1}`;
+            QUANTITY[STOCK_SET].textContent = `${Number(QUANTITY[STOCK_SET].textContent) - 1}`            
+        }
+        else {
+            ID_ARR = ID_ARR.filter((el: string) => {
+                return el !== `${CARD_ID}`;
+            });
+            REMOVE_ITEM.remove();
+        }
+        localStorage.basket = JSON.stringify(ID_ARR);
+        if (!JSON.parse(localStorage.basket).length) {
+            CART.classList.add('visually-hidden');
+            CART.innerHTML = '';
+            TOTAL_SUM.innerHTML = '0';
+        }
+        if (JSON.parse(localStorage.basket).length) {
+            CART.classList.remove('visually-hidden');
+            CART.innerHTML = `${JSON.parse(localStorage.basket).length}`;
+            TOTAL_SUM.innerHTML = `${JSON.parse(localStorage.basket).reduce((sum: number, current: string) => {
+                if (CARDS.find((item) => item.id === Number(current))) {
+                sum = sum + (CARDS.find((item) => item.id === Number(current))as cardType)?.price;
+                }
+                return sum;
+            }, 0)}`;
+        }
+        SUMMARY_PRODUCT.textContent = CART.textContent;
+        SUMMARY_TOTAL.textContent = `€ ${TOTAL_SUM.textContent}`;
+    };
+}
